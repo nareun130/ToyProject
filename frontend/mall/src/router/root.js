caf0fb0e306bea897d61d9b1ffd50dc8a92524d8
김성호 <nareun130@gmail.com>
2024-02-05 00:58:58 +0900

    feat : todoRouter 생성 및 list, add 페이지 이동 구현 => useNavigate, useSearchParam, useParam 사용

--- frontend/mall/src/router/root.js
+++ frontend/mall/src/router/root.js
@@ -1,10 +1,13 @@
 import { Suspense, lazy } from "react";
 import {createBrowserRouter} from "react-router-dom";
+import todoRouter from "./todoRouter";
 
 const Loading = <div>Loading....</div>
-const Main = lazy(() => import("../pages/MainPage"))
 
+const Main = lazy(() => import("../pages/MainPage"))
 const About = lazy(() => import("../pages/AboutPage"))
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+const TodoList = lazy(() => import("../pages/todo/ListPage"))
 
 const root = createBrowserRouter([
     {
@@ -14,6 +17,11 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+        children: todoRouter()
     }
 ]);
 
