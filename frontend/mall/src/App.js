c02bf369c5f423424d5f528d0a8c1c099f5ec335
김성호 <nareun130@gmail.com>
2024-02-05 21:16:17 +0900

    feat : git 충돌

--- frontend/mall/src/App.js
+++ frontend/mall/src/App.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import root from './router/root';
 
 function App() {
   return <RouterProvider router={root}/>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return <h1 className='text-3xl font-bold underline'>
-    Hello world!
-  </h1>;
->>>>>>> dev/backend
 }
 
 export default App;
