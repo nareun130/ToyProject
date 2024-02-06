088bd96416d7c23c95668b48b05a05ab1dbb7a18
김성호 <nareun130@gmail.com>
2024-02-06 14:22:31 +0900

    feat : todo List페이지 및 컴포넌트 추가

--- frontend/mall/src/pages/todo/ListPage.js
+++ frontend/mall/src/pages/todo/ListPage.js
@@ -1,4 +1,5 @@
 import {useSearchParams} from "react-router-dom";
+import ListComponent from "../../components/todos/ListComponent";
 
 const ListPage = () => {
 
@@ -12,6 +13,7 @@ const ListPage = () => {
       <div className="text-3xl font-extrabold">
         Todo List Page Component {page} --- {size}
       </div>
+      <ListComponent/>
     </div>
   );
 };
