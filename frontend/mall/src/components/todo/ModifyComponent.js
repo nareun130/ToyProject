b10139e6e5d2cb4e559b1fd20589f8e991b30311
김성호 <nareun130@gmail.com>
2024-02-06 16:54:35 +0900

    feat : 수정, 삭제 후 결과 modal 이동

--- frontend/mall/src/components/todo/ModifyComponent.js
+++ frontend/mall/src/components/todo/ModifyComponent.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { deleteOne, getOne, putOne } from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
+import ResultModal from "../common/ResultModal"
 
 const initState = {
   tno: 0,
@@ -13,26 +15,36 @@ const ModifyComponent = ({ tno, moveList, moveRead }) => {
   const [todo, setTodo] = useState({ ...initState });
 
   //* 모달 창을 위한 상태
-  const [result, setResult] = useState(null)
-  
+  const [result, setResult] = useState(null);
+
+  const {moveToList, moveToRead } = useCustomMove();
+
   useEffect(() => {
     getOne(tno).then((data) => setTodo(data));
   }, [tno]);
 
-  const handleClickModify = () => { 
-
-    putOne(todo).then(data => {
-        console.log('modify result :' + data)
-    })
-  }
+  const handleClickModify = () => {
+    putOne(todo).then((data) => {
+      // console.log("modify result :" + data);
+      setResult('Modified')
+    });
+  };
 
   const handleClickDelete = () => {
+    deleteOne(tno).then((data) => {
+      // console.log("delete result : " + data);
+      setResult('Deleted')
+    });
+  };
 
-    deleteOne(tno).then(data => {
-        console.log("delete result : " + data)
-    })
+  const closeModal = () => {
+    if(result === 'Deleted') {
+      moveToList();
+    } else {
+      moveToRead(tno)
+    }
   }
-  
+
   const handleChangeTodo = (e) => {
     todo[e.target.name] = e.target.value;
 
@@ -49,6 +61,7 @@ const ModifyComponent = ({ tno, moveList, moveRead }) => {
 
   return (
     <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+      {result ? <ResultModal title={'처리결과'} content={result} callbackFn={closeModal}></ResultModal>:<></>}
       <div className="flex justify-center mt-10">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
           <div className="w-1/5 p-6 text-right font-bold">TNO</div>
@@ -104,12 +117,16 @@ const ModifyComponent = ({ tno, moveList, moveRead }) => {
         </div>
       </div>
       <div className="flex justify-end p-4">
-        <button className="rounded p-4 m-2 text-xl w-32 text-white bg-red-500"
-        onClick={handleClickDelete}>
+        <button
+          className="rounded p-4 m-2 text-xl w-32 text-white bg-red-500"
+          onClick={handleClickDelete}
+        >
           Delete
         </button>
-        <button className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500"
-        onClick={handleClickModify}>
+        <button
+          className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500"
+          onClick={handleClickModify}
+        >
           Modify
         </button>
       </div>
