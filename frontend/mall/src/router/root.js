5035e8785e0e8e8570440dce918922ed82a03833
김성호 <nareun130@gmail.com>
2024-02-11 17:31:21 +0900

    feat : 상품 관련 indexPage 추가 및 router 설정

--- frontend/mall/src/router/root.js
+++ frontend/mall/src/router/root.js
@@ -1,13 +1,14 @@
 import { Suspense, lazy } from "react";
 import {createBrowserRouter} from "react-router-dom";
 import todoRouter from "./todoRouter";
+import productsRouter from "./productsRouter";
 
 const Loading = <div>Loading....</div>
 
 const Main = lazy(() => import("../pages/MainPage"))
 const About = lazy(() => import("../pages/AboutPage"))
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
-const TodoList = lazy(() => import("../pages/todo/ListPage"))
+const ProductsIndex = lazy(() => import("../pages/products/IndexPage"))
 
 const root = createBrowserRouter([
     {
@@ -22,6 +23,10 @@ const root = createBrowserRouter([
         path: "todo",
         element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
         children: todoRouter()
+    },{
+        path:"products",
+        element:<Suspense fallback={Loading} ><ProductsIndex/></Suspense>,
+        children: productsRouter()
     }
 ]);
 
