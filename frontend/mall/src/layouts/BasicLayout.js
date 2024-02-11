81e2e242202ab874202886ef4f43b787172cba96
김성호 <nareun130@gmail.com>
2024-02-11 17:57:23 +0900

    feat : 상품 ListPage 추가 및 Router 수정 및 BasicLayout 수정

--- frontend/mall/src/layouts/BasicLayout.js
+++ frontend/mall/src/layouts/BasicLayout.js
@@ -7,8 +7,8 @@ const BasicLayout = ({ children }) => {
 
       <div
         className="bg-white w-full flex flex-col space-y-1 md:flex-row
-      md:space-x-1 md:space-y-0"
-      >
+      md:space-x-1 md:space-y-0 mt-5"
+      > 
         
         <main className="bg-sky-300 md:w-4/5 lg:w-3/4 px-5 py-5">
           {children}
