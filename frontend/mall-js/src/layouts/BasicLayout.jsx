4a3b94d71a62dade386bd9d689a22de514a93183
김성호 <nareun130@gmail.com>
2024-04-22 17:24:04 +0900

    feat : 장바구니 관련 slice, hook, 컴포넌트 추가

--- frontend/mall-js/src/layouts/BasicLayout.jsx
+++ frontend/mall-js/src/layouts/BasicLayout.jsx
@@ -1,4 +1,5 @@
 import BasicMenu from "../components/menus/BasicMenu";
+import CartComponent from "../components/menus/CartComponent";
 
 const BasicLayout = ({ children }) => {
   return (
@@ -17,7 +18,9 @@ const BasicLayout = ({ children }) => {
         <aside className="bg-green-300 md:w-1/5 lg:w-1/4 px-5 flex py-5">
           {" "}
           {/* 상단 여백 py-40 제거 flex 제거 */}
-          <h1 className="text-2xl md:text-4xl">Sidebar</h1>
+          <h1 className="text-2xl md:text-4xl">
+            <CartComponent />
+          </h1>
         </aside>
       </div>
     </>
