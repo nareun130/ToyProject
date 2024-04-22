68f4b053c0f23b733e81bbb65fc2f4d20a2a9c99
김성호 <nareun130@gmail.com>
2024-04-22 11:50:29 +0900

    feat : 이미지 문제 해결

--- backend/mallapi/src/main/java/com/zerock/mallapi/controller/ProductController.java
+++ backend/mallapi/src/main/java/com/zerock/mallapi/controller/ProductController.java
@@ -31,12 +31,12 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/api/products")
 public class ProductController {
 
-  private final ProductService productService; //ProductServcie 주입 
+  private final ProductService productService; // ProductServcie 주입
   private final CustomFileUtil fileUtil;
 
   @PostMapping("/")
-  public Map<String, Long> register(ProductDTO productDTO){
-    
+  public Map<String, Long> register(ProductDTO productDTO) {
+
     log.info("rgister: " + productDTO);
 
     List<MultipartFile> files = productDTO.getFiles();
@@ -47,21 +47,21 @@ public class ProductController {
 
     log.info(uploadFileNames);
 
-    //서비스 호출 
+    // 서비스 호출
     Long pno = productService.register(productDTO);
 
     return Map.of("result", pno);
   }
 
-
   @GetMapping("/view/{fileName}")
-  public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
-
+  public ResponseEntity<Resource> viewFileGET(@PathVariable(name = "fileName") String fileName) {
+    log.info("fileName : " + fileName);
     return fileUtil.getFile(fileName);
 
   }
 
-  // @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")//? 임시 권한 설정 -> MemberDTO에서 ROLE_을 붙여줌.
+  // @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")//? 임시 권한 설정 ->
+  // MemberDTO에서 ROLE_을 붙여줌.
   @PreAuthorize("hasAnyRole('ROLE_USER')")
   @GetMapping("/list")
   public PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO) {
@@ -69,52 +69,52 @@ public class ProductController {
     log.info("list............." + pageRequestDTO);
 
     return productService.getList(pageRequestDTO);
-    
+
   }
 
   @GetMapping("/{pno}")
-  public ProductDTO read(@PathVariable(name="pno") Long pno) {
+  public ProductDTO read(@PathVariable(name = "pno") Long pno) {
 
     return productService.get(pno);
   }
 
   @PutMapping("/{pno}")
-  public Map<String, String> modify(@PathVariable(name="pno")Long pno, ProductDTO productDTO) {
+  public Map<String, String> modify(@PathVariable(name = "pno") Long pno, ProductDTO productDTO) {
 
-    productDTO.setPno(pno); 
+    productDTO.setPno(pno);
 
     ProductDTO oldProductDTO = productService.get(pno);
 
-    //기존의 파일들 (데이터베이스에 존재하는 파일들 - 수정 과정에서 삭제되었을 수 있음)  
+    // 기존의 파일들 (데이터베이스에 존재하는 파일들 - 수정 과정에서 삭제되었을 수 있음)
     List<String> oldFileNames = oldProductDTO.getUploadFileNames();
-    
-    //새로 업로드 해야 하는 파일들  
+
+    // 새로 업로드 해야 하는 파일들
     List<MultipartFile> files = productDTO.getFiles();
 
-    //새로 업로드되어서 만들어진 파일 이름들
+    // 새로 업로드되어서 만들어진 파일 이름들
     List<String> currentUploadFileNames = fileUtil.saveFiles(files);
 
-    //화면에서 변화 없이 계속 유지된 파일들 
+    // 화면에서 변화 없이 계속 유지된 파일들
     List<String> uploadedFileNames = productDTO.getUploadFileNames();
 
-    //유지되는 파일들  + 새로 업로드된 파일 이름들이 저장해야 하는 파일 목록이 됨  
-    if(currentUploadFileNames != null && currentUploadFileNames.size() > 0) {
+    // 유지되는 파일들 + 새로 업로드된 파일 이름들이 저장해야 하는 파일 목록이 됨
+    if (currentUploadFileNames != null && currentUploadFileNames.size() > 0) {
 
       uploadedFileNames.addAll(currentUploadFileNames);
 
     }
-    //수정 작업 
+    // 수정 작업
     productService.modify(productDTO);
 
-    if(oldFileNames != null && oldFileNames.size() > 0){
+    if (oldFileNames != null && oldFileNames.size() > 0) {
 
-      //지워야 하는 파일 목록 찾기 
-      //예전 파일들 중에서 지워져야 하는 파일이름들 
-      List<String> removeFiles =  oldFileNames
-      .stream()
-      .filter(fileName -> uploadedFileNames.indexOf(fileName) == -1).collect(Collectors.toList());
+      // 지워야 하는 파일 목록 찾기
+      // 예전 파일들 중에서 지워져야 하는 파일이름들
+      List<String> removeFiles = oldFileNames
+          .stream()
+          .filter(fileName -> uploadedFileNames.indexOf(fileName) == -1).collect(Collectors.toList());
 
-      //실제 파일 삭제 
+      // 실제 파일 삭제
       fileUtil.deleteFiles(removeFiles);
     }
     return Map.of("RESULT", "SUCCESS");
@@ -123,8 +123,8 @@ public class ProductController {
   @DeleteMapping("/{pno}")
   public Map<String, String> remove(@PathVariable("pno") Long pno) {
 
-    //삭제해야할 파일들 알아내기 
-    List<String> oldFileNames =  productService.get(pno).getUploadFileNames();
+    // 삭제해야할 파일들 알아내기
+    List<String> oldFileNames = productService.get(pno).getUploadFileNames();
 
     productService.remove(pno);
 
@@ -133,7 +133,5 @@ public class ProductController {
     return Map.of("RESULT", "SUCCESS");
 
   }
-
-
 
 }
